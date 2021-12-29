3cfc7eba6c6ffa4a8d77dda883f5992edd8a2449
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-28 22:38:16 -0300

    component sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import {Flex} from "@chakra-ui/react"
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Desboard(){
     return(
-        <Header/>
+        <Flex direction="column" h="100vh">
+            <Header/>
+
+            <Flex w="100%" my="6" maxWidth={1400} mx="auto" px="6">
+                <Sidebar/>
+            </Flex>
+        </Flex>
     )
 }
