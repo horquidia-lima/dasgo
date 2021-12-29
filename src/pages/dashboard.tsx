37884e897a1c0f5bcee60d51d93c94e4dc9265b2
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-28 23:25:07 -0300

    pagina dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -17,6 +17,42 @@ const options = {
         },
         foreColor: theme.colors.gray[500],
     },
+    grid: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    tooltip: {
+        enabled: false,
+    },
+    xaxis: {
+        type: "datetime",
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            "2021-03-18T00:00:00.000Z",
+            "2021-03-19T00:00:00.000Z",
+            "2021-03-20T00:00:00.000Z",
+            "2021-03-21T00:00:00.000Z",
+            "2021-03-22T00:00:00.000Z",
+            "2021-03-23T00:00:00.000Z",
+            "2021-03-24T00:00:00.000Z",
+        ],
+    },
+    fill: {
+        opacity: 0.3,
+        type: "gradient",
+        gradient: {
+            shade: "dark",
+            opacityFrom: 0.7,
+            opacityTo: 0.3,
+        }
+    }
 }
 
 const series = [
@@ -36,6 +72,7 @@ export default function Desboard(){
                         p="8"
                         bg="gray.800"
                         borderRadius={8}
+                        pb="4"
                     >
                         <Text fontSize="lg" mb="4">Inscritos da semana</Text>
                         <Chart options={options} series={series} type="area" height={160}/>
@@ -44,8 +81,10 @@ export default function Desboard(){
                         p="8"
                         bg="gray.800"
                         borderRadius={8}
+                        pb="4"
                     >
                         <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                        <Chart options={options} series={series} type="area" height={160}/>
                     </Box>
                 </SimpleGrid>
             </Flex>
